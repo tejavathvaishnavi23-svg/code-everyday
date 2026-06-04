fc1ea9d7c75b537d1d0c773e21102fb5f1bd333d
Tejavath Vaishnavi <tejavathvaishnavi23@gmail.com>
2026-06-04 22:37:31 +0530

    Trapping rain water

--- rain_water_trapping.py
+++ rain_water_trapping.py
@@ -27,3 +27,43 @@ def water_trapping(arr):
 
 arr = [4, 2, 5, 7, 4, 2, 3, 6, 8, 2, 3]
 print(water_trapping(arr))
+
+#another method
+def water_trapping(heights):
+    n = len(heights)
+
+    prefix = [0] * n
+    suffix = [0] * n
+
+    # Prefix max
+    prefix[0] = heights[0]
+    i = 1
+    while i < n:
+        prefix[i] = max(prefix[i - 1], heights[i])
+        i += 1
+
+    # Suffix max
+    suffix[n - 1] = heights[n - 1]
+    i = n - 2
+    while i >= 0:
+        suffix[i] = max(suffix[i + 1], heights[i])
+        i -= 1
+
+    # Calculate trapped water
+    total_water = 0
+    i = 0
+    while i < n:
+        leftmax = prefix[i]
+        rightmax = suffix[i]
+        waterlevel = min(leftmax, rightmax)
+
+        if waterlevel > heights[i]:
+            total_water += waterlevel - heights[i]
+
+        i += 1
+
+    return total_water
+
+
+arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+print(water_trapping(arr))
