2d035946ec2a5854ddaafa86624add749f108aab
Tejavath Vaishnavi <tejavathvaishnavi23@gmail.com>
2026-06-24 19:32:36 +0530

    Backtracking problem

--- backtracking.py
+++ backtracking.py
@@ -1,7 +1,8 @@
-def fun(path):
-    print(path)
-    path.append(1)
-    print(path)
-    path.pop()
-    print(path)
-fun([])
+nums = [1,2]
+def backtrack(i, subset):
+    if i == len(nums):
+        print(subset)
+        return
+    backtrack(i+1, subset+[nums[i]])
+    backtrack(i+1, subset)
+backtrack(0, [])
