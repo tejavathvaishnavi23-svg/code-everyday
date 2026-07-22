0f86cd97e87520795a7c0a2f6d8b839eec9e8c1b
Tejavath Vaishnavi <tejavathvaishnavi23@gmail.com>
2026-07-22 10:42:50 +0530

    Modular Arithematic Problem

---
+++ modular_arithematic.py
@@ -0,0 +1,16 @@
+def count_pairs(arr,m):
+    freq = [0]*m
+    count = 0
+    for num in arr:
+        rem = num % m
+
+        if rem == 0:
+            pair = 0
+        else:
+            pair = m-rem
+        count += freq[pair]
+        freq[rem] += 1
+    return count
+arr = [5, 2, 3, 4, 6]
+m = 3
+print(count_pairs(arr,m))
